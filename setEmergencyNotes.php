226739dab8ac1ea743c0d7757aed3990d95eb406
Chester Frazier <cwfrazier@cwfrazier.com>
2020-12-02 00:56:37 +0000

    Emergency notes saving correctly

--- setEmergencyNotes.php
+++ setEmergencyNotes.php
@@ -10,8 +10,8 @@
 	$currentMedicalInsurance = $_POST['currentMedicalInsurance'];
 	$specialNotes = $_POST['specialNotes'];
 
-	$notes['currentMedicalInsurance'] = $currentMedicalInsurance;
-	$notes['specialNotes'] = $specialNotes;
+	$notes[0]['currentMedicalInsurance'] = $currentMedicalInsurance;
+	$notes[0]['specialNotes'] = $specialNotes;
 
 	$result = $ddb->updateItem(['ExpressionAttributeNames' => ['#Y' => 'emergencyNotes',],'ExpressionAttributeValues' => [':y' => ['S' => json_encode($notes),],],'Key' => ['id' => ['S' => $id,],],'TableName' => 'accounts','UpdateExpression' => 'SET #Y = :y',]);
 
